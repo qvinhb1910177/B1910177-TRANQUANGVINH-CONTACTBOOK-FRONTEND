9541cfd35cd4697bddca1a194a462404eca34d7b
qvinhb1910177 <marcoving1606@gmail.com>
2023-03-25 18:52:16 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,18 @@ const routes = [
 path: "/",
 name: "contactbook",
 component: ContactBook,
+
 },
+{
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+    },
+    
 ];
+
 const router = createRouter({
+
 history: createWebHistory(import.meta.env.BASE_URL),
 routes,
 });
